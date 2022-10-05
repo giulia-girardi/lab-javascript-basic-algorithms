edf8879cf5f1eabdac590d3a0e60961df9b60106
Giulia Girardi <gg.giuliagirardi@gmail.com>
2022-10-05 12:32:19 +0200

    Finish iteration2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
+console.log(`I'm ready!`)
 // Iteration 1: Names and Input
-//
+
+const hacker1 = "Giulia";
+console.log(`The driver's name is ${hacker1}`);
+const hacker2 = "Eric";
+console.log(`The navigator's name is ${hacker2}`);
+
 // Iteration 2: Conditionals
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else if (hacker2.length > hacker1.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+} else {
+    console.log(`Wow, you both have equally long names, ${hacker2.length} characters!`);
+}
 
 
 // Iteration 3: Loops
