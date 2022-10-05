ffc2d351d756e57c5ebbbcc411b268030ef9a8b9
Giulia Girardi <gg.giuliagirardi@gmail.com>
2022-10-05 12:34:21 +0200

    Finish iteration1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
+console.log(`I'm ready!`)
 // Iteration 1: Names and Input
-//
+
+const hacker1 = "Giulia";
+console.log(`The driver's name is ${hacker1}`);
+const hacker2 = "Eric";
+console.log(`The navigator's name is ${hacker2}`);
+
 // Iteration 2: Conditionals
 
 
